b01162be2653383423e51b7cb11380c662a11598
kim mondelli <kimmondelli@kims-Air.hsd1.ca.comcast.net>
2021-09-12 17:54:58 -0700

    first holy communion

---
+++ index.js
@@ -0,0 +1,6 @@
+import * as d3 from './web_modules/d3.js'
+
+console.log('Hello World')
+
+console.log(d3)
+d3.select('.target').style('stroke-width', 8) // change their style: stroke width is not equal to 8 pixels
